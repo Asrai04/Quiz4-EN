ecc211b0083d8847702469edff3d528bfe2c0244
Asrai04 <a.saborio.1@estudiantec.cr>
2024-10-22 08:17:09 -0600

    Add project files.

---
+++ Quiz4-EN/Quiz4_EN.py
@@ -0,0 +1,34 @@
+import random, time
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped: break
+    return arr
+
+
+i = 0
+lista = []
+while(i <= 10000):
+    h = random.randrange(0, 1000)
+    lista.append(h)
+    i = i+1
+
+
+print(lista)
+inicio = time.time()
+
+# Código a medir
+# -------------
+
+print(bubble_sort(lista))
+
+time.sleep(1)
+
+fin = time.time()
+print(fin-inicio) # 1.0005340576171875
